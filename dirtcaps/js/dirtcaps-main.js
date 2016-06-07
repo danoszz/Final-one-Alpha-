49ae421d40c3b0c5c4e5fba9762598f16ba2b59d
Daan <daan@dvdzdesign.nl>
2016-06-07 15:49:02 +0200

    Grid popput + blurrr

--- dirtcaps/js/dirtcaps-main.js
+++ dirtcaps/js/dirtcaps-main.js
@@ -1,15 +1,40 @@
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-// init Isotope
-var grid = document.querySelector('.grid');
+//Hover trilll
 
-var msnry = new Masonry( grid, {
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
+/* Thanks to http://tympanus.net/codrops/2016/05/11/distorted-button-effects-with-svg-filters/ */
+
+
+var turbVal = { val: 0.000001 };
+var turb = document.querySelectorAll('#filter feTurbulence')[0];
+
+var btTl = new TimelineLite({ paused: true, onUpdate: function() {
+  turb.setAttribute('baseFrequency', '0.000001 ' + turbVal.val);
+} });
+
+
+
+btTl.to(turbVal, 0.4, { val: 0.4 });
+btTl.to(turbVal, 0.2, { val: 0.000001 });
+/*
+
+$('warp').addEventListener('mouseover', function() {
+  btTl.restart();
+    console.log('hoi');
+  });
+*/
+
+$( ".warp:nth-child(1)" ).mouseover(function() {
+    btTl.restart();
 });
 
-imagesLoaded( grid ).on( 'progress', function() {
-  // layout Masonry after each image loads
-  msnry.layout();
+$( ".warp:nth-child(2)" ).mouseover(function() {
+    btTl.restart();
 });
+$( ".warp:nth-child(3)" ).mouseover(function() {
+
+    btTl.restart();
+});
+$( ".warp:nth-child(4)" ).mouseover(function() {
+    btTl.restart();
+});
+
