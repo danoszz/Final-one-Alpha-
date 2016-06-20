c0f9043a2ff8810a1d1fe7a8c0b0a0bf873a7dc6
Daan <daan@dvdzdesign.nl>
2016-06-20 10:05:52 +0200

    Safari Bug: Bounce blur

--- bounce/js/dvdz_bounce.js
+++ bounce/js/dvdz_bounce.js
@@ -1,80 +1,109 @@
-/* 	
+/*
 // From Stephen Zuniga
 // http://codepen.io/stezu/pen/cmLrI
 */
 
 (function ($, window, undefined) {
-	$.fn.marqueeify = function (options) {
-		var settings = $.extend({
-			horizontal: true,
-			vertical: true,
-			speed: 100, // In pixels per second
-			container: $(this).parent(),
-			bumpEdge: function () {}
-		}, options);
-		
-		return this.each(function () {
-			var containerWidth, containerHeight, elWidth, elHeight, move, getSizes,
-				$el = $(this);
+    $.fn.marqueeify = function (options) {
+        var settings = $.extend({
+            horizontal: true,
+            vertical: true,
+            speed: 100, // In pixels per second
+            container: $(this).parent(),
+            bumpEdge: function () {}
+        }, options);
 
-			getSizes = function () {
-				containerWidth = settings.container.outerWidth();
-				containerHeight = settings.container.outerHeight();
-				elWidth = $el.outerWidth();
-				elHeight = $el.outerHeight();
-			};
+        return this.each(function () {
+            var containerWidth, containerHeight, elWidth, elHeight, move, getSizes,
+                $el = $(this);
 
-			move = {
-				right: function () {
-					$el.animate({left: (containerWidth - elWidth)}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
-						settings.bumpEdge();
-						move.left();
-					}});
-				},
-				left: function () {
-					$el.animate({left: 0}, {duration: ((containerWidth/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
-						settings.bumpEdge();
-						move.right();
-					}});
-				},
-				down: function () {
-					$el.animate({top: (containerHeight - elHeight)}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
-						settings.bumpEdge();
-						move.up();
-					}});
-				},
-				up: function () {
-					$el.animate({top: 0}, {duration: ((containerHeight/settings.speed) * 1000), queue: false, easing: "linear", complete: function () {
-						settings.bumpEdge();
-						move.down();
-					}});
-				}
-			};
+            getSizes = function () {
+                containerWidth = settings.container.outerWidth();
+                containerHeight = settings.container.outerHeight();
+                elWidth = $el.outerWidth();
+                elHeight = $el.outerHeight();
+            };
 
-			getSizes();
+            move = {
+                right: function () {
+                    $el.animate({
+                        left: (containerWidth - elWidth)
+                    }, {
+                        duration: ((containerWidth / settings.speed) * 1000),
+                        queue: false,
+                        easing: "linear",
+                        complete: function () {
+                            settings.bumpEdge();
+                            move.left();
+                        }
+                    });
+                },
+                left: function () {
+                    $el.animate({
+                        left: 0
+                    }, {
+                        duration: ((containerWidth / settings.speed) * 1000),
+                        queue: false,
+                        easing: "linear",
+                        complete: function () {
+                            settings.bumpEdge();
+                            move.right();
+                        }
+                    });
+                },
+                down: function () {
+                    $el.animate({
+                        top: (containerHeight - elHeight)
+                    }, {
+                        duration: ((containerHeight / settings.speed) * 1000),
+                        queue: false,
+                        easing: "linear",
+                        complete: function () {
+                            settings.bumpEdge();
+                            move.up();
+                        }
+                    });
+                },
+                up: function () {
+                    $el.animate({
+                        top: 0
+                    }, {
+                        duration: ((containerHeight / settings.speed) * 1000),
+                        queue: false,
+                        easing: "linear",
+                        complete: function () {
+                            settings.bumpEdge();
+                            move.down();
+                        }
+                    });
+                }
+            };
 
-			if (settings.horizontal) {
-				move.right();
-			}
-			if (settings.vertical) {
-				move.down();
-			}
+            getSizes();
 
-      // Make that shit responsive!
-      $(window).resize( function() {
-        getSizes();
-      });
-		});
-	};
+            if (settings.horizontal) {
+                move.right();
+            }
+            if (settings.vertical) {
+                move.down();
+            }
+
+            // Make that shit responsive!
+            $(window).resize(function () {
+                getSizes();
+            });
+        });
+    };
 })(jQuery, window);
 
-$(document).ready( function() {
 
-	$('.marquee').marqueeify({
-		speed: 300,
-		bumpEdge: function () {
-			var newColor = "hsl(" + Math.floor(Math.random()*360) + ", 100%, 50%)";
-			$('.marquee .logo').css('fill', newColor);
-		}
-	});
+$(document).ready(function () {
+
+    $('.marquee').marqueeify({
+        speed: 200,
+        bumpEdge: function () {
+            var newColor = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
+            $('.marquee .logo').css('fill', newColor);
+        }
+    });
 });
